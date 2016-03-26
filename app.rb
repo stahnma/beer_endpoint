8d5fd3d3996801669b4d497b20eb996245a04ce2
Eric Sorenson <eric.sorenson@puppetlabs.com>
2016-03-26 10:42:29 -0700

    Goddamn it.

--- app.rb
+++ app.rb
@@ -6,10 +6,10 @@ require 'sinatra'
 require 'open-uri'
 
 # this is the second tab of the spreasheet, for testing changes to the input data
-SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY/export?format=tsv&id=1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY&gid=2047460198'
+#SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY/export?format=tsv&id=1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY&gid=2047460198'
 
 # this is the 'live' tap list
-#SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY/export?format=tsv&id=1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY&gid=0'
+SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY/export?format=tsv&id=1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY&gid=0'
 BEER_FIELDS =  [:tap, :brewery, :beer_name, :style, :abv, :ibu, :link, :tap_date]
 DEFAULT_VALS = ["Tap 16", "Empty", "Sadness", "Bud Lite Ultra Lime", "n/a",
                 "n/a", "http://untappd.com", "1/12/1997"]

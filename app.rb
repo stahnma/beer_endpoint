668188d87962afd35f37d9ad275dc9425015eb94
Eric Sorenson <eric.sorenson@puppetlabs.com>
2016-03-26 00:18:54 -0700

    Input validation for the app
    This commit moves the input validation out of the view and into the logic. It also adds some checking of each field, with a sane-but-goofy default if there's nothing in the spreadsheet for that value.

--- app.rb
+++ app.rb
@@ -5,7 +5,8 @@ require 'json'
 require 'sinatra'
 require 'open-uri'
 
-SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY/export?format=tsv&id=1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY&gid=0'
+SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY/export?format=tsv&id=1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY&gid=2047460198'
+#SPREADSHEET_URL = 'https://docs.google.com/spreadsheets/d/1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY/export?format=tsv&id=1SipVaaHNzAbI0F4C-wk_TPxxf8lVG4r4q-nqsI-uQhY&gid=0'
 BEER_FIELDS = [:tap, :brewery, :beer_name, :style, :abv, :ibu, :link, :tap_date]
 
 set :port, 8334
@@ -22,12 +23,30 @@ get '/api/v1/beer' do
   whats_on_tap(session).to_json
 end
 
+def validate(input,default)
+  if input.nil? or input.empty?
+    default
+  else
+    input
+  end
+end
+
 def whats_on_tap(session)
   rows = open(SPREADSHEET_URL).each_line
 
   rows.drop(1).map do |row|
     values = row.split("\t").map(&:strip)
-
-    Hash[BEER_FIELDS.zip(values)]
+    v = []
+    # :tap, :brewery, :beer_name, :style, :abv, :ibu, :link, :tap_date
+    v[0] = validate(values[0],"Tap 16")         # green dragon secret tap
+    v[1] = validate(values[1],"Empty")
+    v[2] = validate(values[2],"Sadness")
+    v[3] = validate(values[3],"Bud Lite Ultra Lime")
+    v[4] = validate(values[4],"n/a")          # ABV
+    v[4].gsub!('%','')     # strip % to avoid confusing erb
+    v[5] = validate(values[5],"n/a")          # IBU
+    v[6] = validate(values[6],"http://untappd.com/")
+    v[7] = validate(values[7],"1/12/1997")
+    Hash[BEER_FIELDS.zip(v)]
   end
 end
